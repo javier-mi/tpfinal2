40716e4333e0004ce6c3cb36fed98cb5dc5a73dc
mta <mta@linuxmint>
2019-09-22 20:34:34 -0300

    User modified.

--- tpfinal2-master/src/main/java/edu/caece/tpfinal/domain/User.java
+++ tpfinal2-master/src/main/java/edu/caece/tpfinal/domain/User.java
@@ -29,5 +29,36 @@ public class User {
 	
 	@Column
 	private String description;
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
