3195f389c5f5a07f5154140f3183ecbecc78a1a6
mngonzalezalvarez <mngonzalezalvarez@NBK-09>
2019-09-25 10:00:21 -0300

    [SPRINT3] - Funcionamiento de camara

--- tpfinal2-master/client/src/app/app-routing.module.ts
+++ tpfinal2-master/client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
 
 const routes: Routes = [
-	  { path: '', redirectTo: '/users-list', pathMatch: 'full' },
+		{ path: '', redirectTo: '/login', pathMatch: 'full' },
+		{
+	    path: 'home',
+	    component: HomeComponent
+		},
+		{
+	    path: 'login',
+	    component: LoginComponent
+	  },
 	  {
 	    path: 'users-list',
 	    component: UsersListComponent
